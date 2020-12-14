13766b008a8078db947428333f52af750610e5f8
Chirag Vaswani <chiragvaswani@Chirags-MacBook-Air.local>
2020-12-14 14:16:07 +0530

    Created the node and add functions

---
+++ trie.js
@@ -0,0 +1,22 @@
+function makeNode(ch) {
+  this.ch = ch;
+  this.isTerminal = false;
+  this.map = {}; // store a mapping from character
+  this.words = []; // list of words
+}
+
+function add(str, i, root) {
+  if (i === str.length) {
+    root.isTerminal = true;
+    return;
+  }
+
+  if (!root.map[str[i]])
+    // check if the character we want to add already exists
+    root[str[i]] = new makeNode(str[i]);
+
+  root.words.push(str); // add the word to the list
+  add(str, i + 1, root[str[i]]);
+}
+
+function search(str, i, root) {}
